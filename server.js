b3758074635d7fb7993abe5748f3e15032551231
Patric Jansson <paddy@kth.se>
2018-11-07 16:13:36 +0100

    Added _monitor _about

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const validation = require('./validation')
+const templates = require("./modules/templates");
 const app = express()
 const port = 3000
 
 // Force to accept only json requests
 app.use(bodyParser.json({
-    type: function() {
+    type: function () {
         return true;
     }
 }));
@@ -18,6 +19,28 @@ app.post('/:application/:schema', (request, response) => {
 app.get('/:application/:schema', (request, response) => {
     response = validation.getSchema(request, response)
 })
+
+/**
+ * About route.
+ */
+app.get("/_about", function (req, res) {
+    res.status(200).send(templates._about());
+});
+
+/**
+ * Health check route.
+ */
+app.get("/_monitor", function (req, res) {
+    res.set("Content-Type", "text/plain");
+    res.status(200).send(templates._monitor());
+});
+
+/**
+ * Default route, if no other route is matched.
+ */
+app.use(function (req, res) {
+    res.status(404).send(templates.error404());
+});
 
 app.listen(port, (err) => {
     if (err) {
